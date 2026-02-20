a8a97148d74ec7293042f775693c36e414dd5d3d
Shahriyor <rshahriyor@Mac.mra.com>
2026-02-20 06:14:34 -0500

    implemented role for each user

--- apps/web/src/routes/protected-route.tsx
+++ apps/web/src/routes/protected-route.tsx
@@ -1,11 +1,20 @@
 import { Navigate, Outlet } from "react-router";
 import { authClient } from "@/lib/auth-client";
 
-const ProtectedRoute = () => {
+interface RoleRouteProps {
+    allow: string[];
+};
+
+const ProtectedRoute = ({ allow }: RoleRouteProps) => {
     const { data, isPending } = authClient.useSession();
 
     if (isPending) return <div>Loading...</div>;
+
     if (!data?.user) return <Navigate to="/login" replace />;
+
+    const role = data.user.role ?? "user";
+    if (allow && !allow.includes(role)) return <Navigate to="/*" replace />;
+
     return <Outlet />;
 }
 
